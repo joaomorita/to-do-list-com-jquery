c2f4d3b211b72d98ddac2a2fa0469b5318ab8164
Morita <joao@teste.com>
2024-05-08 11:09:00 -0300

    att

--- script.js
+++ script.js
@@ -2,28 +2,19 @@ $(document).ready(function(){
     var data = new Date(Date.now());
     $('#data-hoje').text(`${data.getUTCDate().toString().padStart(2, '0')}/${(data.getUTCMonth()+1).toString().padStart(2, '0')}/${data.getUTCFullYear()} - ${data.getHours()}:${data.getMinutes()}`);
 
-    $('#btn-submit').click(function(){
+    $('#input-tarefa').on('submit', function(e){
+        e.preventDefault();
         if($('#txtatv').val() == ""){
             alert('[ERRO] Digite uma tarefa no campo abaixo!')
         }else{
             const novaTarefa = $('#txtatv').val();
-            const novoItem = $(`<div class="list"><li  style="">${novaTarefa}</li>
-            <p class="confirma"><img id="btn-confirma" src="images/confirma-branco.png" alt=""></p>
-            <img class="deleta" id="btn-deleta" src="images/lixeira-branca.png" alt=""></div>`);
+            const novoItem = $('<li class="list"></li>');
+            $(`<a href="#">${novaTarefa}</a>`).appendTo(novoItem)
             $(novoItem).appendTo('ul')
-            $(novoItem).fadeIn(2000)
             $('#txtatv').val('')
-            $('#txtatv').focus()
 
-            
-            $(".confirma").on( "click", function() {
-                $('li').each(function(){
-                    $(this).toggleClass( "checked" );
-                })
-            });
-
-            $('.deleta').on("click", function(){
-                $('.list').remove()
+            $('#lista-de-tarefas').on('click', 'li', function () {
+                $(this).find('a').toggleClass('checked');
             })
         }
         
